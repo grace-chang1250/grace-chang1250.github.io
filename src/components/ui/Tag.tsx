4b4767216fe232c73bded0dfe2a72c0492eb0249
Grace Chang <gmchang@umass.edu>
2026-08-10 14:37:25 -0400

    Wipe repo contents per user request

--- src/components/ui/Tag.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Tag.module.css';
-
-type TagProps = {
-  children: string;
-};
-
-export function Tag({ children }: TagProps) {
-  return <span className={styles.tag}>{children}</span>;
-}
